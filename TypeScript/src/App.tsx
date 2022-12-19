2ac953db9224830b3d1349c2088c917f36dc1bea
NakuraTakatsugu <arukan.b.b@gmail.com>
2022-12-20 02:27:38 +0900

    [add]五目並べアプリの基礎を追加

---
+++ TypeScript/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import React from "react";
+import ReactDOM from "react-dom";
+import { GameApp } from './component/Game';
+import './App.css'
+
+export const App = () => {
+  return (
+    <GameApp />
+  )
+}
